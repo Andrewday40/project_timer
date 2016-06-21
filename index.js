f953a1ceed3e796993495ca4eb71315f68a40d37
Andrew <andrewday40@gmail.com>
2016-06-21 19:51:23 -0400

    user story 1 completed

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var server = express();
 
 server.use(express.static(__dirname+'/public'));
 
-server.get('/', function(response, request){
-  response.sendFile('public/html/index.html', {root: __dirname});
+server.get('/', function(request, response){
+  response.sendFile('public/html/index.html', { root: __dirname });
 });
 
 server.listen(8080);
